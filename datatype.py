a51f7e0eda60a5a5fc513c9b83b20e54fed909e8
ROOBIN <erika00yesid@gmail.com>
2024-08-01 19:37:00 -0500

    Tipos de datos en numpy

---
+++ datatype.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+#arr=np.array([1,2,3,4], dtype='float64') #Con float se convirtio en un dato tipo decimal. Sin este era un arreglo de números enteros
+#arr.dtype #Imprime el tipo de dato 
+#print(arr.dtype)
+#print(arr)
+#arr=np.array([1,2,3,4])#Es un arreglo de numeros enteros
+#print (arr)
+#arr = arr.astype(np.float64)#Se convierte en un arreglo de decimales
+#print(arr)
+#arr=np.array([1,2,3,4])
+#arr= arr.astype(np.bool_) # lo convierte en booleano
+#print (arr)
+#arr=np.array([1,2,3,4])
+#arr= arr.astype(np.str_) # Lo convierte en string
+#print (arr)
+arr=np.array(['1','2','3','4'])
+arr= arr.astype(np.int64) #lo convierte en entero
+print (arr)
