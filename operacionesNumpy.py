54cfde0f031556aca193d8db5d86074c8756c963
ROOBIN <erika00yesid@gmail.com>
2024-08-29 18:37:00 -0500

    operaciones numpy

---
+++ operacionesNumpy.py
@@ -0,0 +1,9 @@
+import numpy as np
+lista=[1,2]
+print(lista)
+print(lista*2) #DUPLICA LA LOSTA
+arr=np.arange(0,11)
+arr2=arr.copy()
+print(arr*2)#MULTIPLICA CADA NUMERO DEL ARRAY POR 2
+print(arr+2)#SUMA 2 A CADA NUMERO DEL ARRAY 
+print(11/arr)#DIVIDE LOS NUMERO QUE PUEDE DIVIDIR ENTRE 11
